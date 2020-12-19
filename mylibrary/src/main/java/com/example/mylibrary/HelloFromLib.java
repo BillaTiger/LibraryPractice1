d71a4d7fdaafe6d20f87194e3ea948ebe606acd1
BillaTiger <freetrialfirebase302@gmail.com>
2020-12-19 23:36:46 +0500

    dsfdd

--- mylibrary/src/main/java/com/example/mylibrary/HelloFromLib.java
+++ mylibrary/src/main/java/com/example/mylibrary/HelloFromLib.java
@@ -2,5 +2,9 @@ package com.example.mylibrary;
 
 public class HelloFromLib {
     public static void ShowDialog(){}
-    public static void ShowDialog12(){}
+    public static void ShowDialog1212(){}
+    public static void ShowDialog12122(){}
+    public static void ShowDialog121223(){}
+    public static void ShowDialog1212234(){}
+    public static void ShowDialog12122345(){}
 }
